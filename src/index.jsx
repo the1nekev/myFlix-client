2b1ec3e26cfa31c24da07e1c95770ef2c1c87f69
the1nekev <kevcorcino15@gmail.com>
2023-06-14 10:39:16 -0400

    3.2 Building with JS

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import {createRoot} from 'react-dom/client';
+
+//Import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
+
+//main component (will eventually use all the others)
+const MyFlixApplication = () => {
+    return (
+        <div className="my-flix">
+            <div>Good Morning!</div>
+        </div>
+    );
+};
+
+
+//Finds the root of your app 
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+//Tells react to render your app in the root DOM element
+root.render(<MyFlixApplication />); 
