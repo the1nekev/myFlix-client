d96624f710d3e3a6dcfa99b60ba8445d855b404c
the1nekev <kevcorcino15@gmail.com>
2023-06-26 10:59:23 -0400

    exercise 3.5

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://marvelflix1nekev.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then((resonse) => resonse.json())
+    .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+            localStorage.setItem("user", JSON.stringify(data.user));
+            localStorage.setItem("token", data.token);
+            onLoggedIn(data.user, data.token);
+        } else {
+            alert("No such user");
+        }
+    })
+    .catch((e) => {
+        alert("Something went wrong");
+    });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+}
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,44 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
-    
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser:null);
+    const [token, setToken] = useState(storedToken? storedToken:null);
 
     useEffect(() => {
-    fetch("https://marvelflix1nekev.herokuapp.com/movies")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const moviesFromApi = data.map((movie) => {
-            return {
-              _id: movie.id,
-              Title: movie.Title,
-              ImagePath: movie.ImagePath,
-              Description: movie.Description,
-              Genre: {
-                Name: movie.Genre.Name
-              },
-            //   Director: {
-            //     Name: movie.Director.Name
-            //   },
-              Featured: movie.Featured.toString()
-            };
+        if (!token) {
+            return;
+        }
+
+        fetch("https://marvelflix1nekev.herokuapp.com/movies", {
+            headers: {Authorization: `Bearer ${token}`}
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
         });
-        setMovies(moviesFromApi); 
-      });
-  }, []);
+  }, [token]);
+
+    if (!user) {
+        return(
+        <>
+        <LoginView onLoggedIn={(user,token) => {
+            setUser(user);
+            setToken(token);
+        }} />
+        or 
+        <SignupView />
+        </>
+        );
+    }
 
     if (selectedMovie) {
         return <MovieView movie={selectedMovie} onBackClick={() =>{
@@ -51,6 +59,11 @@ export const MainView = () => {
                     }}/>
                 );
             })}
+            <button onClick={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.clear();
+            }}>Logout</button>
         </div>
     );
 };
