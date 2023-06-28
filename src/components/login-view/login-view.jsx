000bbaaedfb06b580d8c8d5e119931d8900239e6
the1nekev <kevcorcino15@gmail.com>
2023-06-28 10:50:32 -0400

    Card, Button, Form
    Added react-bootstrap components to enhance UI.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button, Form} from "react-bootstrap";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
@@ -38,22 +39,30 @@ export const LoginView = ({ onLoggedIn }) => {
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username: <input type="text"
-        value={username}
-        onChange={(e) => {setUsername(e.target.value)}}
-        required
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
         />
-      </label>
-      <label>
-        Password: <input type="text"
-        value={password}
-        onChange={(e) => {setPassword(e.target.value)}}
-        required
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { Row, Col, Button } from "react-bootstrap";
 
 
 export const MainView = () => {
@@ -27,44 +28,39 @@ export const MainView = () => {
         });
   }, [token]);
 
-  if (!user) {
-    return (
+  return (
+    <Row className="justify-content-md-center">
+      {!user ? (
+        <Col md={5}>
+          <LoginView onLoggedIn={(user) => setUser(user)} />
+          or
+          <SignupView />
+        </Col>
+      ) : selectedMovie ? (
+        <Col md={8}>
+          <MovieView
+            style={{ border: "1px solid green" }}
+            movie={selectedMovie}
+            onBackClick={() => setSelectedMovie(null)}
+          />
+        </Col>
+      ) : movies.length === 0 ? (
+        <div>The list is empty!</div>
+      ) : (
         <>
-        <LoginView onLoggedIn={(user, token) =>{
-            setUser(user);
-            setToken(token);
-        }}
-        />
-        or 
-        <SignupView />
-        </> 
-    );
-  }
-
-    if (selectedMovie) {
-        return <MovieView movie={selectedMovie} onBackClick={() =>{
-            setSelectedMovie(null);
-        }}/>
-    }
-
-    if (movies.length === 0) {
-        return <div>The list is empty.</div>
-    }
-
-    return ( 
-        <div>
-            {movies.map((movie) => {
-                return(
-                    <MovieCard key = {movie.id} movie={movie} onMovieClick={(newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}/>
-                );
-            })}
-            <button onClick={() => {
-                setUser(null);
-                setToken(null);
-                localStorage.clear();
-            }}>Logout</button>
-        </div>
-    );
+          {movies.map((movie) => (
+            <Col className="mb-4" key={movie.id} md={3}>
+              <MovieCard
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
+          ))}
+        </>
+      )}
+      
+    </Row>
+  );
 };
